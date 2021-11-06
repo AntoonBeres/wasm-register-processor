8e88c85bc41a969d653761010d9ea697e8a4075d
AntoonBeres <35069455+AntoonBeres@users.noreply.github.com>
2021-11-06 16:10:17 +0100

    faga

--- target/wasm32-unknown-unknown/release/deps/wasm_processor.d
+++ target/wasm32-unknown-unknown/release/deps/wasm_processor.d
@@ -1,8 +1,8 @@
-/home/antoon/Documents/programming/rust/register-processor-wasm/target/wasm32-unknown-unknown/release/deps/wasm_processor.wasm: src/lib.rs src/utils.rs src/proc/mod.rs src/proc/processor.rs src/proc/instruction_set.rs src/proc/parser.rs
+/home/antoon/Documents/programming/rust/test_server/target/wasm32-unknown-unknown/release/deps/wasm_processor.wasm: src/lib.rs src/utils.rs src/proc/mod.rs src/proc/processor.rs src/proc/instruction_set.rs src/proc/parser.rs
 
-/home/antoon/Documents/programming/rust/register-processor-wasm/target/wasm32-unknown-unknown/release/deps/libwasm_processor.rlib: src/lib.rs src/utils.rs src/proc/mod.rs src/proc/processor.rs src/proc/instruction_set.rs src/proc/parser.rs
+/home/antoon/Documents/programming/rust/test_server/target/wasm32-unknown-unknown/release/deps/libwasm_processor.rlib: src/lib.rs src/utils.rs src/proc/mod.rs src/proc/processor.rs src/proc/instruction_set.rs src/proc/parser.rs
 
-/home/antoon/Documents/programming/rust/register-processor-wasm/target/wasm32-unknown-unknown/release/deps/wasm_processor.d: src/lib.rs src/utils.rs src/proc/mod.rs src/proc/processor.rs src/proc/instruction_set.rs src/proc/parser.rs
+/home/antoon/Documents/programming/rust/test_server/target/wasm32-unknown-unknown/release/deps/wasm_processor.d: src/lib.rs src/utils.rs src/proc/mod.rs src/proc/processor.rs src/proc/instruction_set.rs src/proc/parser.rs
 
 src/lib.rs:
 src/utils.rs:
